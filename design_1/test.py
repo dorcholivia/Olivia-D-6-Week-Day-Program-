d12bd282fe1fc1fd4103a9f049bdd054bb963020
dorcholivia <dorcholivia@gmail.com>
2025-07-09 14:22:32 -0400

    I made sure my other folders contain work aswell

---
+++ design_1/test.py
@@ -0,0 +1,20 @@
+print("How are you feeling today?")
+print("What is something you'd like to do?")
+
+print("\nHere are some options:")
+print("1. Get something to eat")
+print("2. Go see a movie")
+print("3. Go bowling")
+
+choice = input("\nType the number of your choice (1, 2, or 3): ")
+
+if choice == "1":
+    print("🍕 Pizza! Let's grab something yummy!")
+elif choice == "2":
+    print("🎬 Movie time! Should we get snacks, anyone?")
+elif choice == "3":
+    print("🎳 Bowling it is! I've been wanting to hit a strike soon.")
+else:
+    print("🤔 I didn’t understand that. Try typing 1, 2, or 3.")
+    
+
