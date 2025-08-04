72884fee455d19c9f9b0f302ecae8bd53da0e936
dorcholivia <dorcholivia@gmail.com>
2025-08-04 14:02:46 -0400

    Today I Finished Html and Css and I got started on my java cript file

---
+++ html_1/script.js
@@ -0,0 +1,29 @@
+console.log("Successfully connected with javascript")
+alert("Welcome to your A.I. friend")
+
+
+let age = 16;
+
+const birthyear= 2009
+
+console.log("My age is ", age)
+
+const THEME = {
+    background: "#f0f8ff",
+    primary: "rgb (47, 4, 219)",
+    accent: "Libertinus Sans, sans-serif",
+};
+
+let userName = "";            // e.g., "Olivia"
+let userMood = "";             // e.g., "happy", "sad", "neutral"
+let focusArea = "";           // e.g.,  "e.g., "discipline", "stress reli"
+let journalEntries = [];      // array of {date, text}
+let lastCheckIn = null;       // timestamp or ISO string
+
+let growthPlan = {
+    area: "",                 // same as focusArea
+    dayTasks: ["", "", ""],   // 3-day mini plan
+    currentDay: 1,            // progress tracker (1-3)
+    completed:false,
+};
+
